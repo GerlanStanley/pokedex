97577f21db3e3be9b884354feec734747943723f
Gerlan Stanley <gerlangerlan@hotmail.com>
2023-07-12 20:34:29 -0300

    Ordenando imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:hive/hive.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
